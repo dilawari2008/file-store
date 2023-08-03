ddf21191f7155e88c51ad4c4c863d2bad3a7213d
dilawari2008 <dilawari2008@gmail.com>
2023-08-03 23:13:17 +0530

    master - getAllFiles

--- src/main/java/com/example/filestore/module/fileHandler/controller/FileController.java
+++ src/main/java/com/example/filestore/module/fileHandler/controller/FileController.java
@@ -2,9 +2,13 @@ package com.example.filestore.module.fileHandler.controller;
 
 import com.example.filestore.module.fileHandler.services.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/app")
@@ -16,6 +20,11 @@ public class FileController {
     @GetMapping("/ping")
     public String pingRoute() {
         return "pong";
+    }
+
+    @GetMapping("/files")
+    public ResponseEntity<List<?>> getAllFiles() {
+        return new ResponseEntity<>(fileService.getAllFiles(), HttpStatus.OK);
     }
 
 
--- src/main/java/com/example/filestore/module/fileHandler/services/FileService.java
+++ src/main/java/com/example/filestore/module/fileHandler/services/FileService.java
@@ -1,14 +1,22 @@
 package com.example.filestore.module.fileHandler.services;
 
+import com.example.filestore.module.fileHandler.domain.FileInfo;
 import com.example.filestore.module.fileHandler.repository.FileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FileService {
 
     @Autowired
     private FileRepository fileRepository;
+
+    public List<FileInfo> getAllFiles() {
+        List<FileInfo> fileInfoList = fileRepository.findAll();
+        return fileInfoList;
+    }
 
 
 }
