74570ee22a5d59c460ae892b3b3f06ba41805cb2
dilawari2008 <dilawari2008@gmail.com>
2023-08-05 22:54:11 +0530

    master - add all crud apis

--- src/main/java/com/example/filestore/module/fileHandler/controller/FileController.java
+++ src/main/java/com/example/filestore/module/fileHandler/controller/FileController.java
@@ -1,20 +1,22 @@
 package com.example.filestore.module.fileHandler.controller;
 
 import com.example.filestore.module.fileHandler.services.FileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/app")
 public class FileController {
+
+    private final Logger log = LoggerFactory.getLogger(FileController.class);
 
     @Autowired
     private FileService fileService;
@@ -30,10 +32,25 @@ public class FileController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity uploadFile(MultipartFile multipartFile) {
-        fileService.uploadFile(multipartFile);
-        return new ResponseEntity("File being uploaded...", HttpStatus.OK);
+    public ResponseEntity uploadFile(@RequestParam("file")MultipartFile multipartFile) {
+        try {
+            fileService.uploadFile(multipartFile);
+            return new ResponseEntity("File being uploaded...", HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity("Input Error", HttpStatus.BAD_REQUEST);
+        }
     }
 
+    @GetMapping("/download/{id}")
+    public ResponseEntity download(@PathVariable("id") Long fileId) {
+        fileService.download(fileId);
+        return new ResponseEntity("Download complete...", HttpStatus.OK);
+    }
+
+    @PostMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long fileId) {
+        fileService.deleteFile(fileId);
+        return new ResponseEntity("Queueing delete...", HttpStatus.OK);
+    }
 
 }
