0125213ebf97fcd55e2af6c1a679a6716d51bf90
dilawari2008 <dilawari2008@gmail.com>
2023-08-05 22:56:17 +0530

    master - added custom queries

--- src/main/java/com/example/filestore/module/fileHandler/repository/FileRepository.java
+++ src/main/java/com/example/filestore/module/fileHandler/repository/FileRepository.java
@@ -2,8 +2,21 @@ package com.example.filestore.module.fileHandler.repository;
 
 import com.example.filestore.module.fileHandler.domain.FileInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public interface FileRepository extends JpaRepository<FileInfo, Long> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "update FileInfo u set u.deleted=true where u.id=?1")
+    void deleteFile(Long fileId);
+
+    @Query(value = "select u from FileInfo u where u.deleted is null or u.deleted=false")
+    List<FileInfo> findAllActive();
 }
