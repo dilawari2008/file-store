cf10f7a4d9382ae73fcf6a9fbb58ab240790b8b0
dilawari2008 <dilawari2008@gmail.com>
2023-08-06 07:40:39 +0530

    master - change properties from .properties to .yml

--- src/main/java/com/example/filestore/FileStoreApplication.java
+++ src/main/java/com/example/filestore/FileStoreApplication.java
@@ -2,8 +2,10 @@ package com.example.filestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.yml")
 public class FileStoreApplication {
 
 	public static void main(String[] args) {
