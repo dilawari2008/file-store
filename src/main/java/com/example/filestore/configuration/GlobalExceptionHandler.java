3e95e1f2a69d01dfcfafc7bfcab79dd5330a9b49
dilawari2008 <dilawari2008@gmail.com>
2023-08-08 01:39:18 +0530

    master - handle request size exceptions

--- src/main/java/com/example/filestore/configuration/GlobalExceptionHandler.java
+++ src/main/java/com/example/filestore/configuration/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.filestore.configuration;
 
 import com.example.filestore.module.fileHandler.controller.FileController;
 import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -17,8 +18,15 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(FileSizeLimitExceededException.class)
     public ResponseEntity<String> handleFileSizeLimitExceeded(FileSizeLimitExceededException ex) {
-        log.error("(handleFileSizeLimitExceeded) file size exceeded");
+        log.error("(handleFileSizeLimitExceeded) file size exceeded"+ ex.toString());
         String errorMessage = "File size exceeds the maximum permitted size.";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
+
+    @ExceptionHandler(SizeLimitExceededException.class)
+    public ResponseEntity<String> handleRequestSizeLimitExceeded(SizeLimitExceededException ex) {
+        log.error("(handleRequestSizeLimitExceeded) request size exceeded" + ex.toString());
+        String errorMessage = "Request size exceeds the maximum permitted size.";
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
     }
 }
