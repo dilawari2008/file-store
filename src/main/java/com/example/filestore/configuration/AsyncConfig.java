51c13c3bfe36f866a6b78459e0d93c4c10a786e0
dilawari2008 <dilawari2008@gmail.com>
2023-08-19 20:04:06 +0530

    master - added AsyncConfig.java

---
+++ src/main/java/com/example/filestore/configuration/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.example.filestore.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+
+public class AsyncConfig {
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("[runnning-async-process]");
+        executor.initialize();
+        return executor;
+    }
+}
